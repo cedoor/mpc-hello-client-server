784a5d88dd57f93a158cca8fd3315ec538bc19a3
cedoor <me@cedoor.dev>
2025-01-16 16:46:40 +0000

    chore: improve ui

--- src/server/main.ts
+++ src/server/main.ts
@@ -2,21 +2,25 @@ import { WebSocketServer } from 'ws';
 import generateProtocol from '../utils/generateProtocol';
 import assert from '../utils/assert';
 import { readFileSync } from 'fs';
+import { Session } from 'mpc-framework';
+
+// Bob's circuit input.
+const NUMBER = 4;
 
 const wss = new WebSocketServer({ port: 8080 });
 
 wss.on('connection', async ws => {
   ws.on('error', console.error);
 
-  let session;
-
   const protocol = await generateProtocol('./src/circuit/main.ts', (filePath: string) => readFileSync(filePath, 'utf8'));
+
+  let session: Session;
 
   ws.on('message', (msg: Buffer) => {
     if (!session) {
       session = protocol.join(
         'bob',
-        { b: 4 },
+        { b: NUMBER },
         (to, msg) => {
           assert(to === 'alice', 'Unexpected party');
 
@@ -24,7 +28,9 @@ wss.on('connection', async ws => {
         },
       );
 
-      session.output().then(console.log);
+      session.output().then(({ main }) => {
+        console.log(`Result: ${main}`);
+      });
     }
 
     session.handleMessage('alice', new Uint8Array(msg));
--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/mpc-hello/',
+  base: '/mpc-hello-client-server/',
   plugins: [],
 });
